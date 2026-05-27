eb522f384ee4d313009ea1d424b8969b6a3dd7ce
sumana <lakku1418@gmail.com>
2026-05-27 07:56:38 +0530

    datatypes

--- 06-even-odd.sh
+++ 06-even-odd.sh
@@ -2,5 +2,8 @@
 echo "enter number"
 read number
 
-if[$number%2 -eq 0]; then
-   echo "$number is even
+if[$((number % 2)) -eq 0]; then
+   echo "$number is even"
+else
+   echo "$number is odd"
+fi
