10a0aa86ede16d2d07b04441f9871789de172d98
sumana <lakku1418@gmail.com>
2026-05-27 07:28:51 +0530

    datatypes

--- 05-data types.sh
+++ 05-data types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "enter your current age"
 read age
-target-age=60
-years-left=$(($target-age-$age))
-echo "you need  $years-left to reach untill $target-age"
+targetage=60
+years-left=$(($targetage-$age))
+echo "you need  $years-left to reach untill $targetage"
