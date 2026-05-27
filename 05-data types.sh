9e972d4dd528dd5def6bc17540911e64f891722a
sumana <lakku1418@gmail.com>
2026-05-27 07:39:08 +0530

    datatypes

--- 05-data types.sh
+++ 05-data types.sh
@@ -4,3 +4,8 @@ read age
 targetage=60
 remaing_years=$(($targetage-$age))
 echo "remaing years $remaing_years to reach untill $targetage"
+if [ $remaing_years -gt 0 ]; then
+    echo "you have $remaing_years years to reach $targetage"
+else
+    echo "you have already reached $targetage"
+fi
