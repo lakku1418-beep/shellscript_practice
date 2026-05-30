c3b1d01d9051f268bd59799b95489e3ed23d8381
sumana <lakku1418@gmail.com>
2026-05-30 17:21:36 +0530

    assignement1 done

--- 09-system_info.sh
+++ 09-system_info.sh
@@ -3,7 +3,7 @@ SERVER_NAME=webserver01
 OS_NAME=$(cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f 2 )
 DISK_USAGE=$(df -h / | tail -1 | awk '{print $3 "used out of " $2}')  
 MEMORY_USAGE=$(free -h | grep Mem | awk '{print $3 " used out of " $2}')
-UPTIME=$(uptime)
+UPTIME=$( uptime | awk -F "," '{print $1}')
 LOG_FILE="/tmp/system_info.log"
 section() {
     echo "------------------------------------"   
