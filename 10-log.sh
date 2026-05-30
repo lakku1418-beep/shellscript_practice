cb13bd0dc04b7c6c6b3c360d3743275e422cd88d
sumana <lakku1418@gmail.com>
2026-05-30 18:05:31 +0530

    assignement1 done

---
+++ 10-log.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+LOG_FILE="/tmp/system_info.log"
+./09-system_info.sh &>> $LOG_FILE
+if [ $? -eq 0 ]; then
+    echo "script executed successfully"
+else
+    echo "script failed to execute"
+fi  
