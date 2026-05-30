8019c60d7e0d6d7b8e72f3175e0d4e480e7eda41
sumana <lakku1418@gmail.com>
2026-05-30 14:16:44 +0530

    assignement1 done

--- 08-install.sh
+++ 08-install.sh
@@ -3,7 +3,7 @@
 
 USERID=$(id -u)
 
-if [ $? -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
  echo "Error: This command has to be run with superuser privileges (under the root user on most systems)"
     exit 1  
 fi
