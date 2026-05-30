d36d7288a180ad22d1f693eef5081be03f31cc2a
sumana <lakku1418@gmail.com>
2026-05-30 14:19:32 +0530

    assignement1 done

--- 08-install.sh
+++ 08-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+package_name=$1
 
 USERID=$(id -u)
 
@@ -8,4 +8,17 @@ if [ $USERID -ne 0 ]; then
     exit 1  
 fi
 
+dnf list installed $package_name
+ if [ $? -eq 0 ]; then
+   echo "$package_name already installed"
+    else
+    echo " installing $package_name"
+ fi
+dnf install $package_name -y
+  if  [ $? -eq 0]; then
+    echo "$package_name installed successfully"
+    else
+    echo "failed to install $package_name"
+ fi
+ 
 
