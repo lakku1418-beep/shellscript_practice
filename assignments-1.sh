60bf34cf035ae0aec68f1f16f1c47db06abcbf6c
sumana <lakku1418@gmail.com>
2026-05-30 12:10:59 +0530

    assignement1 done

---
+++ assignments-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello sumana welcome to devops world"
+current_date=$(date)
+echo "current date is $current_date"
+echo "who is running script:" $USER
+echo "which directory the script is running:" $PWD
+echo "which shell is running:" $SHELL
