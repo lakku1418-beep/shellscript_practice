b02b4bebf301c921b447c719ae2de551547f63c2
sumana <lakku1418@gmail.com>
2026-05-30 15:52:41 +0530

    assignement1 done

--- 08-install.sh
+++ 08-install.sh
@@ -2,24 +2,26 @@
 package_name=$1
 
 USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/nginx
+LOGS_FILE=$LOGS_DIR/nginx_install.log
 
 if [ $USERID -ne 0 ]; then
  echo "Error: This command has to be run with superuser privileges (under the root user on most systems)"
     exit 1  
 fi
 VALIDATE() 
-      {
-        if [ $2 -eq 0 ]; then
-             echo "installing $1  ..... SUCCESS" 
-             else 
-             echo "installing $1  ..... FAILED" 
-        fi
+       {
+            if [ $2 -eq 0 ]; then
+                echo "installing $1  ..... SUCCESS" 
+                else 
+                echo "installing $1  ..... FAILED" 
+            fi
       }
 print_header() 
         {
-        echo "===================================================="
-        echo "$1"
-        echo "===================================================="     
+            echo "===================================================="
+            echo "$1"
+            echo "===================================================="     
         }
 
     dnf list installed $package_name
