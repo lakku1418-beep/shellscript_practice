c01a0affc646cae2d5ba0eb5212649195a52ce0c
sumana <lakku1418@gmail.com>
2026-05-30 13:50:59 +0530

    assignement1 done

--- 08-install.sh
+++ 08-install.sh
@@ -8,14 +8,14 @@ if [ $? -ne 0 ]; then
 fi
 dnf list installed $package_name
  if [ $? -eq 0 ]; then
-    echo "$pacage_name already installed"
+    echo "$package_name already installed"
     else
-    echo " install $pacage_name"
+    echo " installing $package_name"
  fi
-dnf install $pacage_name -y
+dnf install $package_name -y
 
     if [ $? -eq 0 ]; then
-    echo "$pacckage_name installed successfully"
+    echo "$package_name installed successfully"
     else
     echo "failed to install $package_name"
     fi
