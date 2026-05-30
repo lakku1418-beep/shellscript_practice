e4fc145e246e71b971b145e4b4df487d27616928
sumana <lakku1418@gmail.com>
2026-05-30 12:35:40 +0530

    assignement1 done

---
+++ assignments-3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+username=$1
+country=$2
+echo "username is $username from $country"
+echo "total number of arguments passed is $#"
+echo "all arguments passed are $@"
+echo "print the script name: $0"
